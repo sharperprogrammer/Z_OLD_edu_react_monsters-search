7eedf43005897f7a6f46df3650a4df65c259e3a2
sharperprogrammer <sharperprogrammer@gmail.com>
2020-02-01 21:33:25 -0500

    Updates

---
+++ precache-manifest.d85d85e76817650d1be0086092a7a219.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6439f8e66388a40915cf9cc68ea9054c",
+    "url": "/edu_react_monsters-search/index.html"
+  },
+  {
+    "revision": "1efff24296a325691011",
+    "url": "/edu_react_monsters-search/static/css/main.a6d202c8.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/edu_react_monsters-search/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/edu_react_monsters-search/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1efff24296a325691011",
+    "url": "/edu_react_monsters-search/static/js/main.656b1e4e.chunk.js"
+  },
+  {
+    "revision": "77ec6ab3b2b2eb445ba5",
+    "url": "/edu_react_monsters-search/static/js/runtime-main.e7b91f6b.js"
+  }
+]);
